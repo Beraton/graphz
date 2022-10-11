bce75cafbba29d0456227435511673e17a4dc17d
donberaton <jakub.kubica@protonmail.com>
2022-10-11 17:15:48 +0200

    remove unused code and imports

--- lib/features/weather/presentation/pages/overview_page.dart
+++ lib/features/weather/presentation/pages/overview_page.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphz/features/weather/presentation/bloc/weather_bloc.dart';
-import 'package:graphz/features/weather/presentation/pages/details_page.dart';
 import 'package:graphz/features/weather/presentation/routes/router.gr.dart';
 import 'package:graphz/features/weather/presentation/widgets/card_content.dart';
 import 'package:graphz/features/weather/presentation/widgets/util/weather_sorting_utils.dart';
